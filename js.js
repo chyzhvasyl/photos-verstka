b22aa83f659d65889a82105f24e83227049af70c
Chyzh Vasyl <vasyachyzh1996@gmail.com>
2018-07-20 22:19:43 +0300

    form is over

--- js.js
+++ js.js
@@ -1,11 +1,36 @@
-
-var validateBtn = document.querySelector('.form .button');
-
-
-
-
 function myForm () {
-
-
-
+    let x=document.forms['form']['name'].value;
+    let y=document.forms['form']['email'].value;
+    let z=document.forms['form']['namel'].value;
+    let form = true;
+    if (x.length === 0){
+        document.getElementById('namef').innerHTML='<div class="red">Введите имя  </div>';
+        document.getElementById('firstName').className = "red";
+        form = false;
+        return false;
+    }
+    else {form = true}
+    if (y.length === 0){
+        document.getElementById('emailf').innerHTML='<div class="red">Введите почту  </div>';
+        document.getElementById('email').className = "red";
+        form = false;
+        return false;
+    }else{form = true}
+    if (z.length === 0){
+        document.getElementById('namel').innerHTML='<div class="red">Введите фамилию  </div>';
+        document.getElementById('lastName').className = "red";
+        form = false;
+        return false;
+    }else{form = true}
+    at=y.indexOf("@");
+    dot=y.indexOf(".");
+    if (at<1 || dot <1){
+        form = false;
+        document.getElementById('emailf').innerHTML='<div class="red">email неправильный </div>';
+        document.getElementById('email').className = "red";
+        return false;
+    }else{form = true}
+    if(form){
+        alert('Very good');
+    }
 }
